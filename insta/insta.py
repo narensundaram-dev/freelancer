1bdf0cf138094cc7c643ae56291c6cc61336483d
Narendran <ngnanasu@cisco.com>
2020-04-04 02:41:17 +0530

    fixed no of posts loaded

--- insta/insta.py
+++ insta/insta.py
@@ -21,6 +21,8 @@ log = logging.getLogger(__file__.split('/')[-1])
 class Instagram:
 
     pattern_mobile = (
+        r"(\+91-)?(\+91)?([7-9]{1})([0-9]{9})",  # India
+        r"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$",  # North america (https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s02.html)
         r"(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})",
     )
     pattern_email = (
@@ -35,12 +37,12 @@ class Instagram:
         log.info("Chrome is started using chromedriver: {}".format(self.conf["CHROME_DRIVER_PATH"]))
 
         self.login()
-        self.posts = []
+        self.posts = set()
         self.users = {}
 
     def login(self):
         self.chrome.get("https://www.instagram.com/accounts/login/")
-        time.sleep(5)
+        time.sleep(3)
         try:
             dom_username = self.chrome.find_element_by_xpath('//*[@name="username"]')
             dom_password = self.chrome.find_element_by_xpath('//*[@name="password"]')
@@ -49,14 +51,21 @@ class Instagram:
             dom_username.send_keys(self.cred["USERNAME"])
             dom_password.send_keys(self.cred["PASSWORD"])
             dom_login_btn.click()
-            time.sleep(5)
+            time.sleep(3)
         except BaseException as err:
             log.error("Error: ", err)
 
     def get_scroll_limit(self):
         limit = int(self.conf["LIMIT"])
-        # 1st page: 36 posts. further pages: 9 posts
-        return math.ceil(1 + ((limit - 36) / 9))
+        # 1st page: 36 posts. further pages: 12 posts
+        return math.ceil(1 + ((limit - 36) / 12))
+
+    def load_posts(self):
+        source = self.chrome.execute_script("return document.documentElement.outerHTML")
+        # soup = BeautifulSoup(self.chrome.page_source, 'html.parser')
+        soup = BeautifulSoup(source, 'html.parser')
+        posts = self.get_posts(soup)
+        self.posts = self.posts.union(posts)
 
     def scroll_to_bottom(self):
         limit, wait = self.get_scroll_limit(), int(self.conf["WAIT"])
@@ -64,10 +73,12 @@ class Instagram:
         log.info("{} no. of seconds will be awaited for posts to be loaded on each scroll".format(wait))
 
         last_height = self.chrome.execute_script("return document.body.scrollHeight")
+        self.load_posts()
         count = 0
         while True:
             self.chrome.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(wait)
+            self.load_posts()
 
             # If the page reaches the bottom and no more posts to load
             new_height = self.chrome.execute_script("return document.body.scrollHeight")
@@ -83,16 +94,14 @@ class Instagram:
             log.info("Instagram page scrolled to page {}".format(count+1))
 
     def get_posts(self, soup):
-        posts = soup.article.contents[2].find_all("a", href=re.compile("^/p/.*"), limit=int(self.conf["LIMIT"]))
-        log.info("{} no of posts has been fetched from Instagram. Scroll limit: {}".format(
-            len(posts), self.get_scroll_limit()))
-        return ["{}{}".format(URL, post.attrs["href"]) for post in posts]
+        posts = soup.article.contents[2].find_all("a", href=re.compile(r"^/p/.*"), limit=int(self.conf["LIMIT"]))
+        return set(["{}{}".format(URL, post.attrs["href"]) for post in posts])
 
     @classmethod
     def get_user_name(cls, post):
         html = requests.get(post).text
         soup = BeautifulSoup(html, "html.parser")
-        data_json = str(soup.find_all("script", text=re.compile("^window._sharedData.*"))[0].string).replace(
+        data_json = str(soup.find_all("script", text=re.compile(r"^window._sharedData.*"))[0].string).replace(
             "window._sharedData = ", "").replace(";", "")
         data = json.loads(data_json)
         return data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["owner"]["username"]
@@ -100,43 +109,65 @@ class Instagram:
     @classmethod
     def get_user_email(cls, bio):
         data = []
-        for pattern in cls.pattern_email:
-            matches = re.findall(pattern, bio)
-            data.extend(matches)
-        return ", ".join(data)
+        try:
+            for pattern in cls.pattern_email:
+                matches = re.findall(pattern, bio)
+                data.extend(matches)
+            return ", ".join(data)
+        except Exception as e:
+            print("\nPattern match failed")
+            print("bio: ", bio)
+            print("data: ", data, "\n")
+        return ""
 
     @classmethod
     def get_user_phone(cls, bio):
-        data = []
-        for pattern in cls.pattern_mobile:
-            matches = re.findall(pattern, bio)
-            data.extend(matches)
-        return ", ".join(data)
+        data = set()
+        try:
+            for pattern in cls.pattern_mobile:
+                # matches = re.findall(pattern, bio)
+                matches = re.finditer(pattern, bio, re.MULTILINE)
+                for match in matches:
+                    data.add(match.group())
+            return ", ".join(list(data))
+        except Exception as e:
+            print("\nPattern match failed")
+            print("bio: ", bio)
+            print("data: ", data, "\n")
+        return ""
 
     @classmethod
     def get_user_gender(cls, bio):
-        if "female" in bio.lower():
+        if " female " in bio.lower():
             return "Female"
-        elif "male" in bio.lower() and "female" not in bio.lower():
+        elif " male " in bio.lower():
             return "Male"
 
-    @classmethod
-    def get_user_info(cls, username):
+    def get_user_info(self, username):
         url = "{}/{}/?__a=1".format(URL, username)
-        log.debug("Fetching user info from url: {}".format(url))
-        response = requests.get(url).json()
+        log.info("Fetching user info from url: {}".format(url))
+
+        # Ignoring requests usage since it redirects the login page sometimes
+        # response = requests.get(url).json()
+        self.chrome.get(url)
+        # time.sleep(0.5)
+        soup = BeautifulSoup(self.chrome.page_source, "html.parser")
+        response = json.loads(soup.body.pre.get_text())
+
         user = response["graphql"]["user"]
         bio = user["biography"]
+        cls = Instagram
         return {
             "name": u"{}".format(user["full_name"]),
             "username": user["username"],
             "count_followers": user["edge_followed_by"]["count"],
 
             # To fetch it from user["biography"] using regex
-            "city": "",
             "gender": cls.get_user_gender(bio),
             "email": cls.get_user_email(bio),
-            "phone": cls.get_user_phone(bio)
+            "phone": cls.get_user_phone(bio),
+            "city": "",
+            "biography": bio,
         }
 
     def get_users(self):
@@ -146,18 +177,12 @@ class Instagram:
             log.info("Started scrapping data from {}".format(url))
             log.info("Instagram page no 1 is initially loaded.")
             self.scroll_to_bottom()
-
-            soup = BeautifulSoup(self.chrome.page_source, 'html.parser')
-
-            # BUG: self.chrome.page_source is not loading all the posts. Max 45 posts are loaded.
-            with open("source.html", "w+") as fobj:
-                fobj.write(self.chrome.page_source)
-
-            self.posts = self.get_posts(soup)
+            log.info("{} no of posts has been fetched from Instagram. Scroll limit: {} pages".format(
+                len(self.posts), self.get_scroll_limit()))
 
             log.info("Started fetching the user information from scrapped posts.")
+            # added for testing purpose. will remove it on moving to prod.
             self.users["narensundaram.dev"] = self.get_user_info("narensundaram.dev")
-
             for post in self.posts:
                 username = self.get_user_name(post)
                 if username not in self.users:
